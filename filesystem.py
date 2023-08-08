0efee28ad92c15b3ba5b552deca52915079d5fb2
HamBrick327 <derpykidgamer@gmail.com>
2023-08-08 14:31:12 -0600

    I do in fact need to find a different solution because the link is expiring

--- filesystem.py
+++ filesystem.py
@@ -6,6 +6,6 @@ import os
 if not os.path.exists("./queue"):
     os.mkdir("./queue")
     os.mkdir("./hardcodedAudio")
-    os.system("wget https://download1076.mediafire.com/58g5msotwoygKzJQA2qaDLiw_8EsPaAlihGFyYKw3yA5sYOnIBMptMygDtGJRGpfnd01ee6HDi4_Q9WLEGbws1LNyemUA64-8vvLPwrcWhmlz7Eeg9FPX4_BWUSTBDUMnQWDVwJVk8sImSPexc1YdRBwNj7qeTs98vLNf-Q/i7gwkmyqsaw8b20/hardcodedAudio.zip")
+    os.system("wget https://download1076.mediafire.com/zjpcdkwdm5dgLP_O-y6GT6Ajl2Tdm-HVkf_EuKBCbqA59O5qsz0AF_rpBXaetaQN4XemNyAA_RKnvJF8rObQ9zTIp11_eJNQsZRSNXVpWOwhxSlN4umce1SW_LdBBcn5pKy8SwfOebuw9ESYuhmpZHxn4Wn9H9rj5k-XQdA/i7gwkmyqsaw8b20/hardcodedAudio.zip")
     os.system("unzip hardcodedAudio.zip -d ./hardcodedAudio")
     os.remove("./hardcodedAudio.zip")
