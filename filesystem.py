068c777ae36c66d5e5c58f3f6841ecadf063631d
HamBrick327 <derpykidgamer@gmail.com>
2023-08-08 14:05:36 -0600

    maybe fixed the one bug causing problems?

--- filesystem.py
+++ filesystem.py
@@ -6,6 +6,6 @@ import os
 if not os.path.exists("./queue"):
     os.mkdir("./queue")
     os.mkdir("./hardcodedAudio")
-    os.system("wget https://download1076.mediafire.com/loc5k90txm5gcDtOyfOYzbTR7wIUEGmEgyuI7DWmNEmxjfMi_Ldr07ONmxrmRFglV34v0pnz3EFnatyfp6SeMJmo3-ID_GU4z1V9dnTxROTJL0JyMixk9ndbU0noHGb07FbTR4zP9rOvgqw_V_V8_7v8m8HGpKsdzhKxRvs/i7gwkmyqsaw8b20/hardcodedAudio.zip")
+    os.system("wget https://download1076.mediafire.com/58g5msotwoygKzJQA2qaDLiw_8EsPaAlihGFyYKw3yA5sYOnIBMptMygDtGJRGpfnd01ee6HDi4_Q9WLEGbws1LNyemUA64-8vvLPwrcWhmlz7Eeg9FPX4_BWUSTBDUMnQWDVwJVk8sImSPexc1YdRBwNj7qeTs98vLNf-Q/i7gwkmyqsaw8b20/hardcodedAudio.zip")
     os.system("unzip hardcodedAudio.zip -d ./hardcodedAudio")
     os.remove("./hardcodedAudio.zip")
