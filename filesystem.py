dcecf184ecee1a5b7727f1e2c64303e865c1f01c
HamBrick327 <derpykidgamer@gmail.com>
2023-08-08 17:04:32 -0600

    mediafire bugfixing

--- filesystem.py
+++ filesystem.py
@@ -1,11 +1,17 @@
-#!/bin/env python3
-
+                                #!/bin/env python3
+from bs4 import BeautifulSoup
 import os
 
 ## check if ./queue directory exists, if not, create it
 if not os.path.exists("./queue"):
     os.mkdir("./queue")
     os.mkdir("./hardcodedAudio")
-    os.system('python3 mfdl.py ./ "https://www.mediafire.com/file/ow8hzyoevukxwkj/hardcodedAudio.zip/file"')
+    os.system('wget "https://www.mediafire.com/file/ow8hzyoevukxwkj/hardcodedAudio.zip/file"')
+
+    soup = BeautifulSoup('file', 'html.parser')
+    button = soup.find('a', id="downloadbutton", href=True)
+    os.system(f"wget {button['href']}")
+    
+
     os.system("unzip hardcodedAudio.zip -d ./hardcodedAudio")
     os.remove("./hardcodedAudio.zip")
