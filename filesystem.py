20036e368fd95980fa4b89e65fb740dd7514669a
HamBrick327 <derpykidgamer@gmail.com>
2023-08-08 16:15:23 -0600

    fixing the mediafire bug

--- filesystem.py
+++ filesystem.py
@@ -6,6 +6,6 @@ import os
 if not os.path.exists("./queue"):
     os.mkdir("./queue")
     os.mkdir("./hardcodedAudio")
-    os.system("wget https://download1076.mediafire.com/71czlj4ptqqg24i4Sh85UB4jUaVYSEHykUTVGTY1dE7bhel2wI1vAhN-BSWivLRmgdg5f5jygahToMvDOCF7lE1vIxcPSIrIUAAdCnbE7cLqqJPSy25DY9LKOUGiS7jaBabsY6MH8nSDrluByhhIpC8CJz_xPMdVzYmWY-o/ow8hzyoevukxwkj/hardcodedAudio.zip")
+    os.system('python3 mfdl.py ./ "https://www.mediafire.com/file/ow8hzyoevukxwkj/hardcodedAudio.zip/file"')
     os.system("unzip hardcodedAudio.zip -d ./hardcodedAudio")
     os.remove("./hardcodedAudio.zip")
