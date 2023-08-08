0437afd4330347a90f8bd44f914353741a2e6867
HamBrick327 <derpykidgamer@gmail.com>
2023-08-08 14:38:15 -0600

    checking what the bug was

--- main.py
+++ main.py
@@ -293,7 +293,8 @@ async def down(ctx):
     print(ctx.message.author.id)
     if str(ctx.message.author.id) == "330520485463064597":
         print("owner detected, bot is changing status")
-        await bot.change_presence(status=disnake.Status.idle, activity=disnake.Game(name="Grooby is down :("))
+        await bot.status("Bot is down :(")
+        # await bot.change_presence(status=disnake.Status.idle, activity=disnake.Game(name="Grooby is down :("))
 
 @bot.command()
 async def up(ctx):
