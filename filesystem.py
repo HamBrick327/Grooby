dd92ce83d0e9a53da5d2d3b924ea51b13163e806
HamBrick327 <derpykidgamer@gmail.com>
2023-08-08 15:13:13 -0600

    fixed the hardcoded audio to also include megalovania

--- filesystem.py
+++ filesystem.py
@@ -6,6 +6,6 @@ import os
 if not os.path.exists("./queue"):
     os.mkdir("./queue")
     os.mkdir("./hardcodedAudio")
-    os.system("wget https://download1076.mediafire.com/zjpcdkwdm5dgLP_O-y6GT6Ajl2Tdm-HVkf_EuKBCbqA59O5qsz0AF_rpBXaetaQN4XemNyAA_RKnvJF8rObQ9zTIp11_eJNQsZRSNXVpWOwhxSlN4umce1SW_LdBBcn5pKy8SwfOebuw9ESYuhmpZHxn4Wn9H9rj5k-XQdA/i7gwkmyqsaw8b20/hardcodedAudio.zip")
+    os.system("wget https://download1076.mediafire.com/71czlj4ptqqg24i4Sh85UB4jUaVYSEHykUTVGTY1dE7bhel2wI1vAhN-BSWivLRmgdg5f5jygahToMvDOCF7lE1vIxcPSIrIUAAdCnbE7cLqqJPSy25DY9LKOUGiS7jaBabsY6MH8nSDrluByhhIpC8CJz_xPMdVzYmWY-o/ow8hzyoevukxwkj/hardcodedAudio.zip")
     os.system("unzip hardcodedAudio.zip -d ./hardcodedAudio")
     os.remove("./hardcodedAudio.zip")
